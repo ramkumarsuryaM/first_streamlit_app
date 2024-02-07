101fc4633aa18623f60e3f62bbd9b7ebdd19101d
ramkumarsuryaM <159231408+ramkumarsuryaM@users.noreply.github.com>
2024-02-07 12:32:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parenst New Healthy Diner')
 streamlit.header('Breakfast menu')
+streamlit.text('Idly and Chutney")
+streamlit.text('Dosa and Chutney")
+streamlit.text('Pongal and Chutney")
