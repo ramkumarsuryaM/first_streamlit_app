1769fb5f3f598c6e26567081f240a821943a9108
ramkumarsuryaM <159231408+ramkumarsuryaM@users.noreply.github.com>
2024-02-19 20:51:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows=get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop():
+streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 
